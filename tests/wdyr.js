338f669a623108371f954025643ca02805cffd82
sam bacha <sam@manifoldfinance.com>
2022-07-22 21:28:20 -0700

    test: why did you render

---
+++ tests/wdyr.js
@@ -0,0 +1,9 @@
+/* @test why did you render */
+import React from 'react'
+
+if (process.env.NODE_ENV === 'development') {
+  const whyDidYouRender = require('@welldone-software/why-did-you-render')
+  whyDidYouRender(React, {
+    trackAllPureComponents: true
+  })
+};
